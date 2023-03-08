138a7f2731193a329963bce5c0a10efc146d40d1
Nbslab <timurnezam2@gmail.com>
2023-03-08 14:22:19 +0300

    style(get_repo_data): Fixed codestyle by review comments

--- src/repo_data_parser/get_repo_data.py
+++ src/repo_data_parser/get_repo_data.py
@@ -30,7 +30,6 @@ def extract_fields_from_repo_url(repository_url: str) -> dict:
     """
     if repository_url.endswith(".git"):
         repository_url = repository_url.replace(".git", "")
-    matches = re.search(r"([a-z]+):\/\/([^/]*)\/([^/]*)\/(.*)", repository_url)
     if matches := re.search(r"([a-z]+):\/\/([^/]*)\/([^/]*)\/(.*)", repository_url):
         name_space = str(matches.group(3))
         project = str(matches.group(4))
@@ -52,19 +51,18 @@ def get_repo_info(url: str) -> dict:
         dict with data gained from requests
     """
 
-    if is_github_repo_url(url):
-        result = extract_fields_from_repo_url(url)
-        g = Github()
-        repo = g.get_repo(f"{result['owner']}/{result['project']}")
-        result["language"] = repo.language
-        result["stars"] = repo.stargazers_count
-        result["watchers"] = repo.watchers_count
-        result["forks"] = repo.forks_count
-        result["issue"] = repo.get_issues().totalCount
-        result["pr"] = repo.get_pulls().totalCount
-        result["description"] = repo.description
-        result["commits"] = repo.get_commits().totalCount
-        result["topics"] = ", ".join(repo.get_topics())
-        return result
-    else:
-        raise ValueError("Not GitHub url provided")
+    if not is_github_repo_url(url):
+        raise ValueError("No GitHub url provided")
+    result = extract_fields_from_repo_url(url)
+    g = Github()
+    repo = g.get_repo(f"{result['owner']}/{result['project']}")
+    result["language"] = repo.language
+    result["stars"] = repo.stargazers_count
+    result["watchers"] = repo.watchers_count
+    result["forks"] = repo.forks_count
+    result["issue"] = repo.get_issues().totalCount
+    result["pr"] = repo.get_pulls().totalCount
+    result["description"] = repo.description
+    result["commits"] = repo.get_commits().totalCount
+    result["topics"] = ", ".join(repo.get_topics())
+    return result
