bad5aa47dd13a408e85ba600f418980e4c05a9fa
Nbslab <timurnezam2@gmail.com>
2023-05-23 07:14:18 +0300

    feat(tokenization): added function to generate embeddings from provided text

---
+++ src/data_preprocessing/tokenization.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def create_embeddings(text: str, lda, count_vect) -> np.ndarray:
+    term_doc_matrix_for_one = count_vect.transform([text])
+    embeddings = lda.transform(term_doc_matrix_for_one)
+    print(embeddings)
+    return embeddings
+
+
+if __name__ == "__main__":
+    create_embeddings("simple javascript library")
